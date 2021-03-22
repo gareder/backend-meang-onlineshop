b9fa3fafac17737e5c3dcff553bdf9ff91754203
Eder Garcia <ederjogarcia@gmail.com>
2021-03-22 18:21:50 -0500

    Getting total users, tags, products, genres for the admin dashboard

--- src/resolves/query/index.ts
+++ src/resolves/query/index.ts
@@ -4,13 +4,15 @@ import resolversShopProductsQuery from './shop-product';
 import resolversGenreQuery from './genre';
 import resolversTagQuery from './tag';
 import queryStripeResolvers from './stripe';
+import resolversDashboardQuery from './dashboard';
 
 const queryResolvers = GMR.merge([
   resolversUserQuery,
   resolversShopProductsQuery,
   resolversGenreQuery,
   resolversTagQuery,
-  queryStripeResolvers
+  queryStripeResolvers,
+  resolversDashboardQuery
 ]);
 
 export default queryResolvers;
